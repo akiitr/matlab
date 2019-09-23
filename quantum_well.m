4eeb8f4f923620eaef5aeedf5189ac24fe73e8bf
akiitr <55649509+akiitr@users.noreply.github.com>
2019-09-24 00:00:08 +0530

    Update quantum_well.m

--- quantum_well.m
+++ quantum_well.m
@@ -1,33 +1,57 @@
 %Quantum well using the finite difference method
 
-%defining boundary condition
-xa=0;
-xb=3;
-fa=0;
-fb=0;
-E=200*10^9;
-l=3*10^(-4);
-w=15*10^3;
-L=3;
-%Grid
-Nx=101;
-dx=(xb-xa)/(Nx-1);
-%Build matrix operation
-%for DX2
-z= sparse(Nx,Nx);
+hbar = 1;
+mass = 1;
+%Domain range
+xa=-20;
+xb=20;
+Nx =1201;%number of points
+dx=(xb-xa)/(Nx-1);%step size
+%for kinetic
+z=sparse(Nx,Nx);
 d= ones(Nx,1);
-A=spdiags(d,-1,z);
-A=spdiags(d,+1,A);
-A=spdiags(-2*d,0,A);
-A=[A]*1/(dx^(2));
-Y=[A]*(E*l);
-%%solve for b
-q=[0:0.03:3]';
-r=q.^2;
-b=q*(w*L/2)-r*(w/2);
-%solve or inverse
-f=Y\b;
-plot (q,f)
+A=spdiags(-d,-1,z);
+A=spdiags(-d,+1,A);
+A=spdiags(2*d,0,A);
+kinetic_multiplier = hbar^2/(2*mass*dx^2);
+A = A*kinetic_multiplier;
+a=5;%for quantum well
+L=30;
+v=2;
+t=0.05;
+x=-L:t:L;
+s=size(x);
+V(1:s(2))=0;
+for i=1:s(2)
+if abs(x(i))<a
+V(i)=-v;
+end
+end
+P=V';
+%for potential
+B=spdiags(P,0,z);
+%the hamiltonian
+hamiltonian = A+B;
+[eigenvectors, eigenvalues] =
+eig(full(hamiltonian));%calculation of eigen values and eigen
+vector
+x1 = linspace(0,dx*Nx,Nx);%initializing x-axis
+ground_state_wavefunction = eigenvectors(:,1) +
+eigenvalues(1,1)*d;
+
+first_excited_state = eigenvectors(:,2) + eigenvalues(2,2)*d;
+second_excited_state = eigenvectors(:,3) + eigenvalues(3,3)*d;
+plot(x1,V,'-k','linewidth',4); %plotting potetntial V
+hold on
+plot(x1, ground_state_wavefunction, 'g', 'LineWidth', 2)%for
+ground state
+hold on
+plot(x1, first_excited_state, 'c', 'LineWidth', 2)
+hold on
+plot(x1, second_excited_state, 'r', 'LineWidth', 2)
+legend('Potential Energy', 'Ground State', 'First Excited
+State', 'Second Excited State')
 xlabel('Position')
-ylabel('Y')
-title('Bending moment diagram')
+ylabel('Energy')
+title(' Wavefunction vs. Position for the First Three Energy
+Levels')
